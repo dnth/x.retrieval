a5e9f12bd82dbf9e4e32c186c79049c71700561a
dnth <dickson.neoh@gmail.com>
2024-11-14 15:51:19 +0800

    the first working notebook

--- xretrieval/datasets/coco.py
+++ xretrieval/datasets/coco.py
@@ -13,16 +13,38 @@ class COCODataset:
 
     def load_annotations(self) -> pd.DataFrame:
         """Load and process COCO annotations."""
-        # Load caption annotations
+        # Load caption and instance annotations
         captions_file = self.annotations_dir / "captions_val2017.json"
+        instances_file = self.annotations_dir / "instances_val2017.json"
+
         with open(captions_file, "r") as f:
             coco_captions = json.load(f)
+        with open(instances_file, "r") as f:
+            coco_instances = json.load(f)
 
         # Create DataFrames
         df_images = pd.DataFrame(coco_captions["images"])
         df_captions = pd.DataFrame(coco_captions["annotations"])
+        df_instances = pd.DataFrame(coco_instances["annotations"])
+        df_categories = pd.DataFrame(coco_instances["categories"])
 
-        # Merge images with captions
+        # Prepare category information
+        df_categories = df_categories.rename(columns={"id": "category_id"})
+        df_instances = df_instances[["image_id", "category_id"]]
+
+        # Get categories per image
+        df_image_categories = pd.merge(
+            df_instances, df_categories, on="category_id", how="left"
+        )
+
+        # Group categories by image
+        df_image_categories = (
+            df_image_categories.groupby("image_id")["name"]
+            .agg(lambda x: ",".join(sorted(list(set(x)))))
+            .reset_index()
+        )
+
+        # Merge everything together
         df_images = df_images.rename(columns={"id": "image_id"})
         df = pd.merge(
             df_images[["file_name", "image_id"]],
@@ -30,6 +52,8 @@ class COCODataset:
             how="left",
             on="image_id",
         )
+
+        df = pd.merge(df, df_image_categories, how="left", on="image_id")
 
         # Add full image paths
         df["image_path"] = df.file_name.apply(lambda x: str(self.images_dir / x))
@@ -48,6 +72,7 @@ class COCODataset:
                     "file_name": "first",
                     "image_path": "first",
                     "caption": lambda x: " ".join(x),
+                    "name": "first",
                 }
             )
             .reset_index()
